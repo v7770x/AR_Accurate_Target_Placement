0f3976a2700a5f344c28743aa593302007422fe8
Vishvam Mazumdar <30667536+v7770x@users.noreply.github.com>
2021-01-05 11:38:41 -0500

    added computation of angles between normal vecotrs

--- PCVisualizerApp/flask-app/app/data_logger.py
+++ PCVisualizerApp/flask-app/app/data_logger.py
@@ -3,12 +3,14 @@ from datetime import datetime
 import os
 import json
 import csv
+import numpy as np
 DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_data") 
+
 
 def save_optimization_data(model_points, detected_points, distances, angles, normal_vectors):
     dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S") 
     n = len(get_optimization_data())
-    pd_dict = {'0_Index': n+1,'1_Timestamp': [dt_string], '2_Model_Points': [model_points], '3_Detected_Points':[detected_points], '4_Distances':[distances], '5_Angles':[angles], '6_Normal_Vectors':[normal_vectors] }
+    pd_dict = {'0_Index': n+1,'1_Timestamp': [dt_string], '2_Model_Points': [model_points], '3_Detected_Points':[detected_points], '4_Distances':[distances], '5_Angles':[np.array(angles)*180/np.pi], '6_Normal_Vectors':[normal_vectors] }
     df = pd.DataFrame(pd_dict)
     optimization_path = os.path.join(DATA_PATH, "optimization_data.csv")
     print("saving to", optimization_path)
